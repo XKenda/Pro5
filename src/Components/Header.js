096efea771e02d3916794c88d8c69cae5a410cb9
XKenda <elmlonxxr@gmail.com>
2025-06-03 14:14:55 +0300

    Add initial CSS styles and font files for Font Awesome
    - Created index.css with base styles and header layout.
    - Added Font Awesome webfont files (TTF and WOFF2) for brands, regular, solid, and v4 compatibility.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,33 @@
-import react from "react";
-
+import React from "react";
+import {BrowserRouter, NavLink } from 'react-router-dom';
+import "../index.css"
+import { FaFacebookF, FaInstagram, FaLinkedinIn, FaGithub  } from "react-icons/fa";
 
 const Header = () => {
 
+    const clickedStyle = (isActive) => {
+        return{
+            color: isActive? "white":"gray",
+        }
+    }
+
+    return(
+        <div className="header-con">
+            <h2 className="header-h2">Portifolio</h2>
+            <nav className="header-ul">
+                <li><a>Home</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#projects">Projects</a></li>
+            </nav>
+            <ul className="header-icons">
+                <li><FaLinkedinIn /></li>
+                <li><FaFacebookF /></li>
+                <li><FaInstagram /></li>
+                <li><FaGithub /></li>
+            </ul>
+            <button className="header-connect-btn">Let's connect</button>
+        </div>
+    )
 }
 
 export default Header;
